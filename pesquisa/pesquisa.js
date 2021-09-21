df3637a2b8e0b099ffae67a9851a16f09656b16c
Pedro Silva <pedro81565@hotmail.com>
2021-09-21 16:30:53 -0300

    footer criado

--- pesquisa/pesquisa.js
+++ pesquisa/pesquisa.js
@@ -8,14 +8,18 @@ $('#pesquisa').keyup(function () {
     //faz uma requisição para api omdb no modo busca que devolve uma array de 10 objetos de filmes
     $.getJSON(`http://www.omdbapi.com/?`, { s: filmesPesquisados, apikey: 'eef5e1d2', type: 'movie' }, function (filmesInfo, statusText, xhr) {
       //verifica e o status da requisição é 200(ok)
-      console.log(filmesInfo)
+      console.log(filmesInfo.Response)
+      if(filmesInfo.Response=="False"){
+        exibirFilme+=`<h2 class="col-md-auto">FIlme não encontrado!</h2>`
+        $('.img-container').html(exibirFilme)
+      }
       //compara se o valor devolvido não é undefined antes de executar o proximo passo
-      if (xhr.status === 200 && filmesInfo.Search !== undefined) {
+      else if (xhr.status === 200 && filmesInfo.Search !== undefined) {
         //percorre o array de objetos devolvidos pela requisição filmes info
         $.each(filmesInfo.Search, function (index, movie) {
           if (movie.Poster !== "N/A" && index < 8) {
             exibirFilme += `<figure class="col-sm-6 col-md-4 col-lg-3" onclick="retorna(id)" id="${movie.imdbID}"data-bs-toggle="modal" data-bs-target="#exampleModal">`
-            exibirFilme += `<img src="${movie.Poster}" class="col-12 movies" value="${movie.imdbID}"  " alt="">`
+            exibirFilme += `<img src="${movie.Poster}" class="col-12 movies" value="${movie.imdbID}" alt="">`
             exibirFilme += `<figcaption>${movie.Title}<br>Ano: ${movie.Year}</figcaption>`
             exibirFilme += `</figure>`
             $('.img-container').html(exibirFilme)
@@ -28,18 +32,63 @@ $('#pesquisa').keyup(function () {
   }
 })
 
+// $('#verMais').click(function(){
+//   console.log('teste')
+//   let page = document.getElementById('verMais').value
+//   const filmesPesquisados = document.getElementById('pesquisa').value
+//   console.log(filmesPesquisados)
+//   let exibirFilme = ''
+//   console.log(page)
+
+  
+
+//   $.getJSON(`http://www.omdbapi.com/?`, { s: filmesPesquisados, apikey: 'eef5e1d2', type: 'movie',page:`${page}` }, function (filmesInfo, statusText, xhr) {
+//       //verifica e o status da requisição é 200(ok)
+//       console.log(filmesInfo)
+//       //compara se o valor devolvido não é undefined antes de executar o proximo passo
+//       if (xhr.status === 200 && filmesInfo.Search !== undefined) {
+//         //percorre o array de objetos devolvidos pela requisição filmes info
+//         $.each(filmesInfo.Search, function (index, movie) {
+//           if (movie.Poster !== "N/A" && index < 10) {
+//             exibirFilme += `<figure class="col-sm-6 col-md-4 col-lg-3" onclick="retorna(id)" id="${movie.imdbID}"data-bs-toggle="modal" data-bs-target="#exampleModal">`
+//             exibirFilme += `<img src="${movie.Poster}" class="col-12 movies" value="${movie.imdbID}" alt="">`
+//             exibirFilme += `<figcaption>${movie.Title}<br>Ano: ${movie.Year}</figcaption>`
+//             exibirFilme += `</figure>`
+//             $('.img-container').append(exibirFilme)
+//           }
+//         }
+//         )
+//       }
+      
+//     }
+//     )
+    
+// })
+
+
+$('#btn-search').click(function () {
+  let exibirFilme = ''
+  const filmesPesquisados = document.getElementById('pesquisa').value
+  console.log(filmesPesquisados)
+  $.getJSON(`http://www.omdbapi.com/?`, { t:filmesPesquisados, apikey: 'eef5e1d2', type: 'movie' }, function (filmeInfo) {
+    exibirFilme += `<figure class="col-md-auto" onclick="retorna(id)" id="${filmeInfo.imdbID}"data-bs-toggle="modal" data-bs-target="#exampleModal">`
+    exibirFilme += `<img src="${filmeInfo.Poster}" class="col-12 movies" value="${filmeInfo.imdbID}"  " alt="">`
+    exibirFilme += `<figcaption>${filmeInfo.Title}<br>Ano: ${filmeInfo.Year}</figcaption>`
+    exibirFilme += `</figure>`
+    $('.img-container').html(exibirFilme)
+
+  })
+})
 
 
 function retorna(id) {
   $.getJSON(`https://www.omdbapi.com/?`, { apikey: 'eef5e1d2', i: id, }, function (dadosFilmes) {
     console.log(dadosFilmes)
     document.getElementById('exampleModalLabel').textContent = `${dadosFilmes.Title}`
-
-
     let detalhesFilmes = `<div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
+        <div class="row">
           <div class="col-md-6" id="card-img">
-            <img src="${dadosFilmes.Poster}" class="img-fluid rounded-start" alt="...">
+            <img src="${dadosFilmes.Poster}" class="img-fluid rounded-start" alt="${dadosFilmes.Title}.jpg">
           </div>
           <div class="col-md-6">
             <div class="card-body">
@@ -56,19 +105,21 @@ function retorna(id) {
           </div>
         </div>
       </div>`
-
     $('.modal-body').html(detalhesFilmes)
-
   })
 }
 
 $(document).on({
-  ajaxStart: function(){
-      $("body").addClass("loading"); 
+  ajaxStart: function () {
+    $("body").addClass("loading");
   },
-  ajaxStop: function(){ 
-      $("body").removeClass("loading"); 
-  }    
+  ajaxStop: function () {
+    
+    $("body").removeClass("loading");
+  }
 });
 
 //(`https://www.omdbapi.com/?`, { apikey: 'eef5e1d2', i: value.imdbID, }, function (dadosFilmes) {
+
+
+
