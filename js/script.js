cb7fa20e4ae36ed9875260615c30241b5759344f
Pedro Junior <40725275+Pedro-jds@users.noreply.github.com>
2021-09-23 11:28:55 -0300

    Revert "Test"

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 0.2,
-  wrap: true
-})
-
-$('#myModal').on(hide.bs.modal , function(){
-  // quando o modal for fechado 
-});
